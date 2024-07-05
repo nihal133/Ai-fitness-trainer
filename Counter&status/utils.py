7c9800af6e47717c8badc6e0c0e998653f2bfbb3
nihal133 <nihaaltarek97@gmail.com>
2024-07-05 23:27:26 +0300

    first commit

---
+++ Counter&status/utils.py
@@ -0,0 +1,46 @@
+
+import mediapipe as mp
+import pandas as pd
+import numpy as np
+import cv2
+
+mp_pose = mp.solutions.pose
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  
+    b = np.array(b)  
+    c = np.array(c)  
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) -\
+              np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+
+def detection_body_part(landmarks, body_part_name):
+    return [
+        landmarks[mp_pose.PoseLandmark[body_part_name].value].x,
+        landmarks[mp_pose.PoseLandmark[body_part_name].value].y,
+        landmarks[mp_pose.PoseLandmark[body_part_name].value].visibility
+    ]
+
+
+def detection_body_parts(landmarks):
+    body_parts = pd.DataFrame(columns=["body_part", "x", "y"])
+
+    for i, lndmrk in enumerate(mp_pose.PoseLandmark):
+        lndmrk = str(lndmrk).split(".")[1]
+        cord = detection_body_part(landmarks, lndmrk)
+        body_parts.loc[i] = lndmrk, cord[0], cord[1]
+
+    return body_parts
+
+
+def score_table( frame ):
+    return frame
+    
---
+++ README.md
@@ -0,0 +1,6 @@
+# AI Fitness Trainer
+
+This repository contains the code and resources for the AI Fitness Trainer app. For additional resources, such as datasets and related documents, please visit the following Google Drive folder:
+
+[AI Fitness Trainer Resources](https://drive.google.com/drive/folders/18dLKIAnklNW7s5FPNrc1ICO1L6cxSnsU)
+"# Ai-fitness-trainer" 
